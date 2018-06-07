65e2317dceddf96a5ce6a5d0507d98391d01410f
HASSAN MALIK <HASSAN MALIK@DESKTOP-HAOFC7N>
2018-06-07 12:00:43 -0700

    Add project files.

---
+++ App4/MainActivity.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace App4
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true, LaunchMode = Android.Content.PM.LaunchMode.SingleTop, Icon = "@drawable/H")]
+    public class MainActivity : AppCompatActivity
+    {
+
+        //int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //.axml r .cs ko jorny k lye h
+            SetContentView(Resource.Layout.main);
+
+
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            if (toolbar != null)
+            {
+                SetSupportActionBar(toolbar);
+                SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+                SupportActionBar.SetHomeButtonEnabled(false);
+            }
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            //var clickButton = FindViewById<Button>(Resource.Id.my_button);
+
+            //clickButton.Click += (sender, args) =>
+            //  {
+            //      clickButton.Text = string.Format("{0} clicks!", count++);
+            //  };
+
+            //first button pr click kry tw ye second page pr lyjay
+            Button firstButton = FindViewById<Button>(Resource.Id.button1);
+            firstButton.Click += FirstButton_Click;
+
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            button2.Click += Button2_Click;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            EditText editText = FindViewById<EditText>(Resource.Id.editText11);
+
+            if(editText.Text == string.Empty)
+            {
+                Toast.MakeText(this, "TextBox is Empty", ToastLength.Short).Show();
+            }
+            else
+            {
+                
+                //ye 3 lines data transfer krny k lye h 
+                //Toast.MakeText(this, "Text Successfully Submit", ToastLength.Long).Show();
+                Intent intentClass = new Intent(this,typeof( secondActivity));
+                intentClass.PutExtra("text", editText.Text);
+                StartActivity(intentClass);
+            }
+        }
+
+        private void FirstButton_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(secondActivity));
+        }
+    }
+}
+
---
+++ App4/secondActivity.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App4
+{
+    [Activity(Label = "secondActivity")]
+    public class secondActivity : Activity
+    {
+        private ArrayAdapter<string> adapter;              // gloobal krdia isko
+        private string disply = "";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.secondlayout);//phly second layout r second acitvity ka relation bnaya ha 1
+
+            string[] name = { "Ali", "Hassan", "Rizwan", "Adeel", "Farry" }; //second pr 5 types array declare ki ha 2
+            ListView emplistview = FindViewById<ListView>(Resource.Id.listView1); //listView ko yha access kia ha 3
+            adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, name);// fifth pr arryadapter ka obj ly k ismy asy kam krna h 5
+            emplistview.Adapter = adapter;//fourth pr jsy data source ko lty ty wse ismy isko lia ha 4
+            emplistview.ItemClick += Emplistview_ItemClick; // six pr emplistview ka event create kia ha 6
+
+
+
+            Button secondButton = FindViewById<Button>(Resource.Id.button1);
+            secondButton.Click += SecondButton_Click;
+
+            //ye nichy wali 3 line ak form sy dosry form sy data recieve krny  k lye hain
+            string textJoMainSyAyga = Intent.GetStringExtra("text"); // string same honi chaia
+            EditText editText = FindViewById<EditText>(Resource.Id.editText1);
+            editText.Text = textJoMainSyAyga;
+        }
+
+        private void Emplistview_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            string name = adapter.GetItem(e.Position);  //ye lazzmi krna ha
+            //string disply = ""; isko gloobal krdia
+            if (name == "Ali")
+            {
+                disply = "Name: Ali Malik";
+            }
+            if (name == "Hassan")
+            {
+                disply = "Name: Hassan Malik";
+
+            }
+            if (name == "Rizwan")
+            {
+                disply = "Name: Rizwan Malik";
+
+            }
+            if (name == "Adeel")
+            {
+                disply = "Name: Adeel Arshad";
+
+            }
+            if (name == "Farry")
+            {
+                disply = "Name: Farrukh Javed";
+
+            }
+            MessageBox("Information", disply);
+        }
+
+        private void MessageBox(string title, string message)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);  //yha .builder k sath lia ha
+            AlertDialog alertDialog = builder.Create(); // yha bger builder k sath obj lia ha
+
+            alertDialog.SetTitle(title);
+            alertDialog.SetMessage(message);
+            alertDialog.SetIcon(Android.Resource.Drawable.IcDialogInfo);
+            alertDialog.SetCancelable(false);
+            alertDialog.SetButton("OK", OKBOtton);
+            alertDialog.Show();
+        }
+
+        private void OKBOtton(object sender, DialogClickEventArgs e)
+        {
+            Toast.MakeText(this, "Enter " + disply + " Record", ToastLength.Long).Show();
+        }
+
+        //goto home wala kam
+        private void SecondButton_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
